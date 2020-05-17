440b9543ccecf8de6e9178b8cab1dbdfb28b8a66
Technote <technote.space@gmail.com>
2020-05-18 02:35:13 +0900

    feat: as component

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, {FC} from 'react';
 import {StoreContextProvider} from './app/Store';
 import App from './app/App';
 import {AppOptions} from './types';
 import useTheme from './app/hooks/useTheme';
 
-export const initialize = (options: AppOptions): void => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <StoreContextProvider options={options}>
-        <App options={options}/>
-      </StoreContextProvider>
-    </React.StrictMode>,
-    document.getElementById('root'),
-  );
+export const GaFramework: FC<{ options: AppOptions }> = ({options}) => {
+  return <StoreContextProvider options={options}>
+    <App options={options}/>
+  </StoreContextProvider>;
 };
 
+export {AppOptions};
 export {IGeneticAlgorithm, GeneticAlgorithmBase} from './app/logic/Algorithm';
 export {useStoreContext, useDispatchContext} from './app/Store';
 export {Graph} from './app/components';
