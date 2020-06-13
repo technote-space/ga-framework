96470ba514373c14b0f5da2443693ea9d33a0ca7
Technote <technote.space@gmail.com>
2020-06-13 17:20:46 +0900

    chore: add init function

--- src/app/logic/Algorithm/GeneticAlgorithmBase.ts
+++ src/app/logic/Algorithm/GeneticAlgorithmBase.ts
@@ -2,5 +2,10 @@ import {AlgorithmBase} from '@technote-space/genetic-algorithms-js';
 import {IGeneticAlgorithm} from '.';
 
 export abstract class GeneticAlgorithmBase<ObjectType> extends AlgorithmBase implements IGeneticAlgorithm<ObjectType> {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any,@typescript-eslint/explicit-module-boundary-types,@typescript-eslint/no-unused-vars
+  public async init(context: any): Promise<void> {
+    //
+  }
+
   public abstract async getObject(): Promise<ObjectType>;
 }
