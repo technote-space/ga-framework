d9b1c6c5438bb37e836fdbfc175412463e5ff8c2
Technote <technote.space@gmail.com>
2020-08-01 14:15:08 +0900

    feat: add list component (#71)

--- src/app/components/index.tsx
+++ src/app/components/index.tsx
@@ -1,10 +1,12 @@
 import Graph from './Graph';
+import List from './List';
 import Switching from './Switching';
 import LoadingAnimation from './LoadingAnimation';
 import SnackbarWrapper from './SnackbarWrapper';
 
 export {
   Graph,
+  List,
   Switching,
   LoadingAnimation,
   SnackbarWrapper,
